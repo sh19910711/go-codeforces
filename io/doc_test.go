457442bb352290df92ae41e6b96497f58aa5d869
Hiroyuki Sano <sh19910711@gmail.com>
2017-07-13 13:08:26 +0000

    io: test 828C input

--- io/doc_test.go
+++ io/doc_test.go
@@ -7,14 +7,26 @@ import (
 	"testing"
 )
 
-func Test810B(t *testing.T) {
+func input(c, p string) string {
 	buf := &bytes.Buffer{}
-	io.Examples(buf, "810", "B", ".input")
-	s := buf.String()
+	io.Examples(buf, c, p, ".input")
+	return buf.String()
+}
+
+func Test810B(t *testing.T) {
+	s := input("810", "B")
 
 	if !strings.Contains(s, "4 2") {
 		t.Errorf("input should contain 4 2")
 	}
+
+	if strings.Contains(s, "<br/>") {
+		t.Errorf("input should not contain <br>")
+	}
+}
+
+func Test828C(t *testing.T) {
+	s := input("828", "C")
 
 	if strings.Contains(s, "<br/>") {
 		t.Errorf("input should not contain <br>")
